2f0b38feb956a3fb127b2f1fa2f45ec6f13dc8e0
yckuo219 <1110058@kids.jgjhs.tyc.edu.tw>
2024-07-01 16:48:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
 // Firebase 配置
 var firebaseConfig = {
-  // 你的 Firebase 配置
+  apiKey: "AIzaSyAMM1u5DTfXDUi-2ZBWx9D5MRjm17jdu_w",
+  authDomain: "chattest-ae184.firebaseapp.com",
+  databaseURL: "https://chattest-ae184-default-rtdb.firebaseio.com",
+  projectId: "chattest-ae184",
+  storageBucket: "chattest-ae184.appspot.com",
+  messagingSenderId: "768325792551",
+  appId: "1:768325792551:web:90917c54fafcd937be2f02",
+  measurementId: "G-9RST1S7RPP"
 };
 firebase.initializeApp(firebaseConfig);
-
-// 初始化 Firebase 服务
-var auth = firebase.auth();
-var database = firebase.database();
-
-// 登录逻辑
-auth.onAuthStateChanged(user => {
-    if (user) {
-        console.log("User logged in:", user.displayName);
-        // 你可以在这里加载用户数据并显示在游戏中
-    } else {
-        auth.signInAnonymously().catch(error => {
-            console.error("Anonymous login failed:", error);
-        });
-    }
-});
 
 // 初始化 Three.js
 var scene = new THREE.Scene();
