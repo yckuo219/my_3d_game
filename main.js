49a1721a155dbd79b5848b90a32476e621f53128
yckuo219 <1110058@kids.jgjhs.tyc.edu.tw>
2024-07-01 17:27:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 // main.js
 
+// Check if Firebase app is already initialized
 if (!firebase.apps.length) {
+    // Your Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyAMM1u5DTfXDUi-2ZBWx9D5MRjm17jdu_w",
+        authDomain: "chattest-ae184.firebaseapp.com",
+        databaseURL: "https://chattest-ae184-default-rtdb.firebaseio.com",
+        projectId: "chattest-ae184",
+        storageBucket: "chattest-ae184.appspot.com",
+        messagingSenderId: "768325792551",
+        appId: "1:768325792551:web:90917c54fafcd937be2f02",
+        measurementId: "G-9RST1S7RPP"
+    };
+
+    // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
 } else {
-    firebase.app(); // 如果已经初始化，则使用现有实例
+    // If Firebase app already initialized, use existing app
+    firebase.app();
 }
 
 const db = firebase.database();
