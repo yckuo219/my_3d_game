79e33ee5e3e2aa95971602dda14f4efe225904a1
yckuo219 <1110058@kids.jgjhs.tyc.edu.tw>
2024-07-01 17:04:24 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAMM1u5DTfXDUi-2ZBWx9D5MRjm17jdu_w",
+  authDomain: "chattest-ae184.firebaseapp.com",
+  databaseURL: "https://chattest-ae184-default-rtdb.firebaseio.com",
+  projectId: "chattest-ae184",
+  storageBucket: "chattest-ae184.appspot.com",
+  messagingSenderId: "768325792551",
+  appId: "1:768325792551:web:90917c54fafcd937be2f02",
+  measurementId: "G-9RST1S7RPP"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
